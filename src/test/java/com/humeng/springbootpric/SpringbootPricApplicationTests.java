ebdbeb5fc2c8bf046177f542332343764b9bef49
humeng <1365794157@qq.com>
2022-03-12 12:46:27 +0800

    自己做的练习

---
+++ src/test/java/com/humeng/springbootpric/SpringbootPricApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humeng.springbootpric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPricApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
